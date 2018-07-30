6770593746442fd88446fe3c33947bb2612f907d
Charles Severance <csev@umich.edu>
2018-07-30 10:37:07 -0400

    More complete cron

--- cron.sh
+++ cron.sh
@@ -2,3 +2,22 @@
 
 echo "I am Cron Cron I Am"
 date
+
+echo ===== Updating main web site 
+cd /var/www/html
+git pull
+
+echo ===== Updating /tsugi
+cd /var/www/html/tsugi
+git pull
+
+echo ==== tsugicloud Database upgrade
+sleep 2
+cd /var/www/html/tsugi/admin
+php upgrade.php
+
+echo ==== Tool pull
+sleep 2
+cd /var/www/html/tsugi/admin/install
+php update.php
+
